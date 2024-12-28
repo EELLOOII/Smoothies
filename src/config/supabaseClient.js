e17a0c8bd652e7c54c47a74081612f76d930d012
eloi <eloicram18@gmail.com>
2024-12-28 14:57:59 +0800

    FIXED: Utilize the '.env' file and use the variables supabaseURL and supabaseKey to abstract the key and the url

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://svmcviaxtxxpuynxwtkv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2bWN2aWF4dHh4cHV5bnh3dGt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4NjI1MTQsImV4cCI6MjA1MDQzODUxNH0._tOMn9T34WeRjB9Q7JlnVJoLyhHHY6hoW3fQaSkgubI'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = process.env.REACT_APP_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
