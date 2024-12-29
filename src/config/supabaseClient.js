8f83f6cb21acb407afd95b00f4d16e23e67205c7
eloi <eloicram18@gmail.com>
2024-12-29 16:24:26 +0800

    CHORE: comment the logging in the console of the supabase key and url that is used for debugging the supabase configuration

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -3,8 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_ANON_KEY
 
-console.log('Supabase URL:', supabaseUrl)
-console.log('Supabase Key:', supabaseKey)
+// console.log('Supabase URL:', supabaseUrl)
+// console.log('Supabase Key:', supabaseKey)
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase URL or Supabase Key')
+}
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
