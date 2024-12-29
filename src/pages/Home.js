2ab4e4af16b9d063eba1c232a1638695d66c2924
eloi <eloicram18@gmail.com>
2024-12-30 00:57:55 +0800

    FEAT: Update the local state of the smoothies base on the id of the deleted smoothie

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,11 @@ const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState(null)
 
+  const handleDelete = (id) => {
+    setSmoothies(prevSmoothies => {
+      return prevSmoothies.filter(smoothie => smoothie.id !== id)
+    })
+  }
 
   useEffect(() => {
     const fetchSmoothies = async () => {
@@ -37,7 +42,11 @@ const Home = () => {
         <div className="smoothies">
           <div className="smoothie-grid">
             {smoothies.map(smoothie => (
-              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+              <SmoothieCard 
+                key={smoothie.id} 
+                smoothie={smoothie} 
+                onDelete={handleDelete}
+                />
             ))} 
           </div>
         </div>
