6d4151e4bbe077c55bed696078554c885bebd428
eloi <eloicram18@gmail.com>
2024-12-31 14:48:23 +0800

    FEAT: Create a custom error page for smoothie id that are not existing in supabase

---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+
+const Error = () => {
+    return (
+      <div className="error-page">
+        <i className="material-icons">warning</i>
+        <p>The <span>Smoothie id</span> is not existing</p>
+      </div>
+    )
+  }
+  
+  export default Error
+  
