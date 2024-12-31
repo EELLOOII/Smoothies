3512ea8cc582c3235c758872a995c1e6a671761b
eloi <eloicram18@gmail.com>
2024-12-31 14:44:36 +0800

    FEAT: Adds an 'order by' functionality to order by the smoothies based on Time Created, Title and Rating

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react"
 import SmoothieCard from "../components/SmoothieCard"
 
 const Home = () => {
-  
+
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState(null)
+  const [orderBy, setOrderBy] = useState('created_at')
 
   const handleDelete = (id) => {
     setSmoothies(prevSmoothies => {
@@ -17,37 +18,44 @@ const Home = () => {
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
-      .from('smoothies')
-      .select()
+        .from('smoothies')
+        .select()
+        .order(orderBy, { ascending: false })
 
       if (error) {
         setFetchError('Could not fetch the smoothies')
         setSmoothies(null)
-        console.log('error: ' + error)        
+        console.log('error: ' + error)
       }
       if (data) {
         setSmoothies(data)
         setFetchError(null)
         console.log('data: ' + data)
       }
-    }    
+    }
 
     fetchSmoothies()
-  }, [])
+  }, [orderBy])
 
-  return (   
+  return (
     <div className="page home">
       {fetchError && (<p>{fetchError}</p>)}
       {smoothies && (
         <div className="smoothies">
+          <div className="order-by-container">
+            <span className="order-by-text">Order by:</span>
+            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+          </div>
           <div className="smoothie-grid">
             {smoothies.map(smoothie => (
-              <SmoothieCard 
-                key={smoothie.id} 
-                smoothie={smoothie} 
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
                 onDelete={handleDelete}
-                />
-            ))} 
+              />
+            ))}
           </div>
         </div>
       )}
